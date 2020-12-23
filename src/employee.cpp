b0f323048a3d7d668053dc8f4018d0561bd91854
nataliabieniek <natalia.bieniek@student.pk.edu.pl>
2020-12-23 12:23:07 +0100

    zmiana na duże litery

--- src/employee.cpp
+++ src/employee.cpp
@@ -3,18 +3,30 @@
 #include <cstdlib>
 #include <cstring>
 
+std::string uppercase(std::string word)
+{
+    std::string wyraz = word;
+     for(int i=0;i<=wyraz.length();i++)
+     {
+        if(wyraz[i]>=97 && wyraz[i]<=122)
+        {
+		    wyraz[i]=wyraz[i]-32;
+        }
+     }
+    return wyraz;
+}
 void employee::set(std::string ID, std::string NAME, std::string SURNAME, std::string DEPARTMENT, std::string POSITION )
 {
-    id = ID;
-    name = NAME;
-    surname = SURNAME;
-    departmentId = DEPARTMENT;
-    position = POSITION;
+    id = uppercase(ID);
+    name = uppercase(NAME);
+    surname = uppercase(SURNAME);
+    departmentId = uppercase(DEPARTMENT);
+    position = uppercase(POSITION);
 }
 void employee::print()
 {
-    std::cout << "ID:\t" << id << std::endl; 
-    std::cout << "Name:\t" << name << std::endl;
+    std::cout << "ID:\t\t" << id << std::endl; 
+    std::cout << "Name:\t\t" << name << std::endl;
     std::cout << "Surname::\t" << surname << std::endl;
     std::cout << "Department ID:\t" << departmentId << std::endl;
     std::cout << "Position:\t" << position << std::endl;
