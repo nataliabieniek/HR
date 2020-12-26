47e08ace34272e92d50d87bd9c7bda5a3eac5151
nataliabieniek <natalia.bieniek@student.pk.edu.pl>
2020-12-26 21:41:22 +0100

    ostatnie poprawki

--- src/main.cpp
+++ src/main.cpp
@@ -19,30 +19,33 @@ int main()
     bool dzialanie=true;
     while(dzialanie)
     {
-        std::cout << "Wybierz jeden z następujących numerów:" << std::endl;
+        std::cout << "Wybierz jeden z nastepujacych numerow:" << std::endl;
         std::cout << "1\t-Wydrukuj wszystkich pracownikow" << std::endl;
         std::cout << "2\t-Wydrukuj ile osob jest w wszystkich departamentach, wraz z id osob tam pracujacych" << std::endl;
         std::cout << "3\t-Wydruj pracownikow danego departamentu" << std::endl;
         std::cout << "4\t-Wydrukuj zarobki wszystkich pracownikow" << std::endl;
         std::cout << "5\t-Wydrukuj posortowane zarobki wszystkich pracownikow" << std::endl;
         std::cout << "6\t-Dodaj nowego pracownika, wraz z jego zarobkami" << std::endl;
-        std::cout << "100\t-Wyjdz z programu" << std::endl;
+        std::cout << "7\t-Wyjdz z programu" << std::endl;
         std::cin  >> wybor;
         std::cout << std::endl;
 
         switch(wybor)
         {
-            case 100:
+            case 7:
             {
                 dzialanie=false;
+                break;
             }
             case 1:
             {
                 przyklad.print_all_workers();
+                break;
             }
             case 2:
             {
                 przyklad.print_connection_dep();
+                break;
             }
             case 3:
             {
@@ -50,14 +53,17 @@ int main()
                 std::cout << "Podaj z jakiego dzialu chcesz znalezc pracownikow" << std::endl;
                 std::cin >> dzial;
                 przyklad.print_department(dzial);
+                break;
             }
             case 4:
             {
                 przyklad.print_salary();
+                break;
             }
             case 5:
             {
                 przyklad.printSalariesSorted();
+                break;
             }
             case 6:
             {
@@ -74,13 +80,16 @@ int main()
                 std::cout << "Podaj pozycje" << std::endl;
                 std::cin >> pos;
                 std::cout << "Podaj pensje" << std::endl;
+                std::cin >> sal;
                 employee nowy(id, name, surname, dep, pos);
                 przyklad.read_from_console(nowy);
                 przyklad.add(nowy, dep, sal);
+                break;
             }
             default:
             {
                 std::cout << "Zle wybrales!" << std::endl;
+                break;
             }
         }
     }
